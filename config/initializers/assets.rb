4284421dd7c6d7d9e77a31018ba58d2128df7f61
Igor Felipe Boniatti <igorboniatti@unochapeco.edu.br>
2018-10-15 10:11:49 -0300

    home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
+Rails.application.config.assets.precompile += %w( styleoriginal.css )
 Rails.application.config.assets.precompile += %w( style.css )
+
+Rails.application.config.assets.precompile += %w( core.min.js )
+Rails.application.config.assets.precompile += %w( script.js )
